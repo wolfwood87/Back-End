51484049cf526efbda38909a3480b84deaf05c71
Derek <legato_5@yahoo.com>
2020-03-01 22:49:27 -0800

    created readme file for ease of use

--- airports/airport-router.js
+++ airports/airport-router.js
@@ -12,10 +12,10 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    const { id } = req.params
-    Airports.findBy({id: id})
+    const { id } = req.params.id
+    Airports.findBy({iata_code: id})
         .then(airport => {
-            res.status(201).json(airport)
+            res.status(200).json(airport)
         })
         .catch(err => {
             res.status(500).json({message: "Could not retrieve airport"})
